4a3c2b33384f0b43088fe1574803386a0fde254f
Daniel Nieto <danielnieto89@gmail.com>
2024-02-25 00:18:17 -0600

    create custom User model without username field

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "linktrove.users",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
