a5f3d76aa9c251d78dd26d221f152d2662b06c06
Daniel Nieto <danielnieto89@gmail.com>
2024-06-04 17:36:29 -0600

    create custom URLWidget

--- linktrove/links/widgets.py
+++ linktrove/links/widgets.py
@@ -1,5 +1,10 @@
 from taggit.forms import TagWidget
+from django.forms import TextInput
 
 
 class TagsWidget(TagWidget):
     template_name = "links/widgets/tags.html"
+
+
+class URLWidget(TextInput):
+    template_name = "links/widgets/url.html"
